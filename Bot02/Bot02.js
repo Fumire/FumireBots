12944cca2c6d0646b2af5c1180f14f83366338c2
Jaewoong Lee <230@fumire.moe>
2020-06-22 19:42:46 +0900

    Add Normal EXP data

--- Bot02/Bot02.js
+++ Bot02/Bot02.js
@@ -14,6 +14,7 @@ const probability = {};
 const typing = {};
 const wordquiz = {};
 const loh = {};
+const loh_data = {};
 
 const kalingModule = require("kalink").Kakao();
 const Kakao = new kalingModule;
@@ -654,14 +655,21 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
         loh[room] = ["", Date.now()];
     }
 
+    if (loh_data["Normal"] == undefined)
+    {
+        loh_data["Normal"] = JSON.parse(FileStream.read("/storage/emulated/0/Bots/Bots/Bot02/LOH/Normal.json"));
+    }
+
     if (msg == "//로오히") {
         replier.reply("//로오히 던전 → 딜 손실을 막기 위해 한 명씩 차례로 들어가요!");
+        return;
     } else if (msg == "//로오히 던전") {
         if (loh[room][0] == "") {
             replier.reply(prefix + "지금 던전(재앙의 경계)에 아무도 입장하지 않았습니다.\n'//로오히 던전 입장' 명령어로 입장해 보세요");
         } else {
             replier.reply(prefix + "지금 " + loh[room] + " 님께서 도전 중이십니다.");
         }
+        return;
     } else if (msg == "//로오히 던전 입장") {
         if (loh[room][0] == "") {
             replier.reply(prefix + sender + " 님께서 던전에 입장하셨습니다.\n나오실 때 '//로오히 던전 퇴장' 잊지 마세요!");
@@ -671,9 +679,57 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
         } else if (loh[room][0] != sender) {
             replier.reply(prefix + sender + "님이 아니라 " + loh[room][0] + " 님께서 도전 중이십니다.");
         }
+        return;
     } else if (loh[room][0] != "" && Date.now() - loh[room][1] > 600 * 1000) {
         replier.reply(prefix + loh[room][0] + "님께서 10분이 지나 퇴장한 것으로 간주되었습니다.");
         loh[room] = ["", Date.now()];
+        return;
+    }
+    else if (msg == "//로오히 경험치")
+    {
+        replier.reply("이렇게 명령해주세요\n→ //로오히 경험치 노말 1-2");
+        return;
+    }
+    else if (msg.startsWith("//로오히 경험치"))
+    {
+        var data = null;
+        var token = msg.split(" ");
+        if (token.length != 4)
+        {
+            replier.reply("이렇게 명령해주세요\n→ //로오히 경험치 노말 1-2");
+        }
+
+        if(token[2] == "노말")
+        {
+            for(var key in loh_data["Normal"])
+            {
+                if(loh_data["Normal"][key]["스테이지"].startsWith(token[3]))
+                {
+                    data = loh_data["Normal"][key];
+                }
+            }
+        }
+        else if(token[2] == "하드")
+        {
+            replier.reply("아직 준비 중이에요.");
+        }
+        else if(token[2] == "엘리트")
+        {
+            replier.reply("아직 준비 중이에요.");
+        }
+        else
+        {
+            replier.reply("이렇게 명령해주세요\n→ //로오히 경험치 노말 1-2");
+        }
+
+        if(data == null)
+        {
+            replier.reply("잘 못 된 스테이지 입니다.");
+        }
+        else
+        {
+            replier.reply("<" + data["스테이지"] + ">\n→ 총 경험치: " + data["총 경험치"] + "\n→ 소비 행동력: " + data["소모 행동력"] + "\n→ 행동력 당 경험치: " + parseFloat(data["행동력 당 경험치"]).toFixed(3));
+        }
     }
 
     if (probability[room] == undefined) {
